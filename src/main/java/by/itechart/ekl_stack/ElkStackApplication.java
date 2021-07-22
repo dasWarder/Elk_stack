798e2381ed5e23bb691dfccee415f1c01fc5073a
dasWarder <Al.94_94@mail.ru>
2021-07-22 14:06:45 +0300

    Update docker-compose

--- src/main/java/by/itechart/ekl_stack/ElkStackApplication.java
+++ src/main/java/by/itechart/ekl_stack/ElkStackApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EklStackApplication {
+public class ElkStackApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EklStackApplication.class, args);
+        SpringApplication.run(ElkStackApplication.class, args);
     }
 
 }
